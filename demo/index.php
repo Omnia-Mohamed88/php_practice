4112df2b5d93bd1929ad2ca44f0c37368e658472
Omnia Salama <Omnia_salama@ETAX.COM.EG>
2024-07-21 02:08:26 +0300

    separating logic from view

--- demo/index.php
+++ demo/index.php
@@ -1,47 +1,14 @@
-<!-- dealing with variables -->
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        /* body{
-        display: grid;
-        place-items: center;
-        height: 100vh;
-        margin: 0;
-        font-family: sans-serif;
-        } */
-    </style>
- </head>
- <body>
-    <?php
-     $Taxes_forms =[
+ <?php
+     $Taxes_forms =[ 
         ['name' => 'Income tax' ,'percentage' => '25%' ],
         ['name' => 'Excise tax' ,'percentage' => '15%' ],
         ['name' => 'Inheritance' ,'percentage' => '30%' ],
         
      ];
 
-     function filter($Items , $Key , $Value ){ // function signature 
-        $Filtered_items =[];
-        foreach ($Items as $Item){
-            if ($Item[$Key] === $Value){
-                $Filtered_items[] = $Item;
-            }
-        }
-        return $Filtered_items;
-     }
-        $Filtered_tax_forms = filter($Taxes_forms , 'percentage' , '30%')
-    ?>
-    <ul>
-        <?php foreach ($Filtered_tax_forms as $Tax_form ) : ?>
-            <li> <?= $Tax_form['name'] ?> </li>
-        <?php endforeach; ?>
-    </ul>
-<h1>
+  
+        $Filtered_tax_forms = array_filter($Taxes_forms , function($Tax_form){
+            return $Tax_form['percentage'] === '30%';
+        });
     
-</h1>
- </body>
- </html>
+   require "index.view.php";
