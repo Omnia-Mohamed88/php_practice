a2ef8d47b7dbe103471be95c40f0ddf3c7812e12
Omnia Salama <Omnia_salama@ETAX.COM.EG>
2024-07-24 12:40:40 +0300

    Intro to Form Validations

--- demo/controllers/create-note.php
+++ demo/controllers/create-note.php
@@ -4,10 +4,20 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 if ($_SERVER['REQUEST_METHOD']== "POST" ){
-$db->query('INSERT INTO notes (body,user_id) VALUES(:body , :user_id)',[
-     'body' => $_POST['body'],
-     'user_id'=>1
-]);
+    $errors=[];
+    if (strlen($_POST['body'])===0){
+     $errors['body'] = "a body is required ";
+    }
+    if (strlen($_POST['body'])>1000){
+        $errors['body'] = "body length should noyr be more than 1000 char";
+    }
+    if (empty($errors)){
+        $db->query('INSERT INTO notes (body,user_id) VALUES(:body , :user_id)',[
+            'body' => $_POST['body'],
+            'user_id'=>1
+       ]);
+       
+       } 
+    }
 
-}
 require 'views/note-create.view.php';
